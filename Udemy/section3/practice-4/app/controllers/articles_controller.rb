ba681f01b9b58c516f863355fb975d6e0f2b57ab
Pannakbhushana <rahulkumarmishra5338@gmail.com>
2024-02-29 14:37:56 +0530

    practice-4

---
+++ Udemy/section3/practice-4/app/controllers/articles_controller.rb
@@ -0,0 +1,43 @@
+class ArticlesController < ApplicationController 
+    def index 
+        @article=Article.all
+    end
+
+    def show 
+        @article=Article.find(params[:id]);
+    end
+
+    def new 
+        @article=Article.new
+    end
+
+    def edit 
+        @article=Article.find(params[:id])
+    end
+
+    def create 
+        @article=Article.new(params.require(:articles).permit(:title, :description))
+        if @article.save 
+            flash[:notice]="Article was created successfully"
+            redirect_to articles_path
+        else
+            render "new"
+        end
+    end
+
+    def update 
+        @article=Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice]="Article was updated successfully"
+            redirect_to articles_path 
+        else
+            render "edit"
+        end
+    end
+
+    def destroy 
+        @article=Article.find(params[:id])
+        @article.destroy 
+        redirect_to articles_path
+    end
+end
